946a0f3e523012ce5fb8c18a480e0afc79960828
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-12 17:19:25 +0300

    init expressapp

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use('/', (_req, res) => {
+  res.send('Hello').status(200);
+});
+
+const server = app.listen(4200, () =>
+  console.log(`Server  started on port ${4200}`),
+);
+
+export { server };
