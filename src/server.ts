bfa27884621278af66b011307ab8649178b9a55c
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-12 20:14:34 +0300

    trying to setups for vercel deploying

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+
+config();
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
 app.use('/', (_req, res) => {
-  res.send('Hello').status(200);
+  res.json({ message: '🔥🚀☄️' });
 });
 
 const server = app.listen(process.env.PORT || 4200, () =>
-  console.log(`Server  started on port ${4200}`),
+  console.log(`Server  started on port ${process.env.PORT || 4200}`),
 );
 
-export default server;
+export { app, server };
