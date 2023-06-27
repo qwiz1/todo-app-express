45dec1ac6749b1b48a235a631f009c581ae5d4e0
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-27 16:48:47 +0300

    + exports

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,13 @@
-import { todo as todoService } from '../services';
+import { auth as authService, todo as todoService } from '../services';
+import { AuthController } from './auth.controller';
 import { TodoController } from './todo.controller';
 
 const todoController = new TodoController({
   todoService,
 });
 
-export { todoController };
+const authController = new AuthController({
+  authService,
+});
+
+export { todoController, authController };
