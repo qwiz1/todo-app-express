a05b471b0d639b73501a1c2ae79269f31a6b5846
Andrii Solohub <andrei.sologub12@gmail.com>
2023-06-27 16:40:43 +0300

    * move initialize db connection

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,22 @@ import express, { json } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import passport from 'passport';
 import { initApi } from './api';
-import { handleErrorMiddleware } from './middlewares';
+import { initializeDBConnection } from './config/database';
+import { getJwtStrategy, handleErrorMiddleware } from './middlewares';
 
 const app = express();
+
+// Connect to PostgresDB
+initializeDBConnection();
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(json());
+app.use(passport.initialize());
+passport.use(getJwtStrategy());
 
 app.get('/', (_req, res) => {
   res.json({ message: '🔥🚀☄️' }).status(200);
